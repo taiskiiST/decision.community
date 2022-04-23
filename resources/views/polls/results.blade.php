d867f299db21daf0996a9f1650cb523c0d7472f8
taiskii <taiskii@mail.ru>
2022-04-23 23:13:13 +0300

    List who don't vote

--- resources/views/polls/results.blade.php
+++ resources/views/polls/results.blade.php
@@ -20,6 +20,7 @@
     <label class="block text-lg text-black font-bold whitespace-wrap">Результаты по {{$poll->name}}</label>
     <div class="flex flex-col hidden lg:-mt-px xl:flex">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 ">
+            @if(auth()->user()->isAdmin())
             <div class="inline-flex">
                 <div class="px-4 py-7 sm:px-6 flex-row-reverse ">
                     <a href="{{route('poll.report_dont_voted',['poll'=>$poll->id])}}"><button type="button" class="justify-end py-2 px-4 border border-transparent text-sm font-medium text-white shadow-sm rounded-md bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500" >
@@ -27,6 +28,7 @@
                         </button></a>
                 </div>
             </div>
+            @endif
             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                 <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                     @foreach($poll->questions as $question)
@@ -99,6 +101,7 @@
 
     <div class="flex flex-col xl:hidden ">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 ">
+            @if(auth()->user()->isAdmin())
             <div class="inline-flex flex-row w-full place-content-between">
                 <div class="px-4 py-7 sm:px-6 flex-row-reverse ">
                     <a href="{{route('poll.report_dont_voted',['poll'=>$poll->id])}}"><button type="button" class="justify-end py-2 px-4 border border-transparent text-sm font-medium text-white shadow-sm rounded-md bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500" >
@@ -106,6 +109,7 @@
                         </button></a>
                 </div>
             </div>
+            @endif
             <div class="py-2 align-middle min-w-full sm:px-1 lg:px-8">
                 <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                     @foreach($poll->questions as $question)
